628fa18a54c2ffb76bc41972a9292168b991307e
Ekaterina <1ekaterina.kovaleva@gmail.com>
2025-11-20 22:27:19 +0100

    Update HistoryScreen.kt

--- app/src/main/java/com/example/ecofriendlychallengeapp/ui/history/HistoryScreen.kt
+++ app/src/main/java/com/example/ecofriendlychallengeapp/ui/history/HistoryScreen.kt
@@ -11,13 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -94,74 +88,3 @@ import com.example.ecofriendlychallengeapp.ui.reusable.BasicDesign
             }
         }
     }
-
-
-    /* BasicDesign {
-        // --- Your History ---
-        Text("Your History", style = MaterialTheme.typography.headlineSmall)
-        HorizontalDivider(thickness = 2.dp)
-        Spacer(modifier = Modifier.height(25.dp))
-
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(entries) { entry ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 4.dp),
-                        elevation = CardDefaults.cardElevation(4.dp)
-                    ) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(12.dp),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Column {
-                                // Preventing the app to crash using ?.let
-                                // Executing this block only if the challengeName and date variables are not 0
-                                entry.challengeName?.let {
-                                    Text(
-                                        it,
-                                        style = MaterialTheme.typography.bodyLarge
-                                    )
-                                }
-                                entry.date?.let {
-                                    Text(
-                                        it,
-                                        style = MaterialTheme.typography.bodyMedium
-                                    )
-                                }
-                            }
-
-                            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                                Icon(
-                                    imageVector = if (entry.success == true) Icons.Default.Check else Icons.Default.Close,
-                                    contentDescription = if (entry.success == true) "Success" else "Missed",
-                                    tint = if (entry.success == true) Color(0xFF2E7D32) else Color(
-                                        0xFFC62828
-                                    )
-                                )
-
-                                IconButton(onClick = { viewModel.deleteEntry(entry) }) {
-                                    Icon(Icons.Default.Delete, contentDescription = "Delete")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-/* @SuppressLint("ViewModelConstructorInComposable")
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun HistoryScreenPreview() {
-    // Creating mock versions so the preview can work
-    val navController = rememberNavController()
-    val fakeViewModel = ChallengeViewModel(Application())
-    HistoryScreen(navController, fakeViewModel)
-}
-*/
